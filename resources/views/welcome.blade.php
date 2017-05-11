c20d4ce80b8dfbf76de2c6caed8cb89c517f4581
atex <atex@qq.com>
2017-05-11 16:41:28 +0800

    添加微信借口模块

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Wechat
                 </div>
 
                 <div class="links">
